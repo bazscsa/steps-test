c7800795127147957b21cbd467934557dd021566
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-08-08 18:18:45 +0200

    Updated variable name

--- step.sh
+++ step.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Searching for changes in tracked files..."
-TEST=$(git status --porcelain)
-if [ -n "$TEST" ] ; then
+MODIFIEDFILES=$(git status --porcelain)
+if [ -n "$MODIFIEDFILES" ] ; then
   echo "There are uncommited files"
 else
   echo "There aren't any uncommited files"
