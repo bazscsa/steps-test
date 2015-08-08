35250f62a4929c20e0ed372d89149b6869785d24
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-08-08 18:05:48 +0200

    changes to checking

--- step.sh
+++ step.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 echo "Searching for changes in tracked files..."
-# git diff --exit-code --quiet
-# if [ $? = 1 ]
-# then
-#   echo "There are uncommited changes"
-# else
-#   echo "There aren't any uncommited changes"
-# fi
 TEST=$(git status --porcelain)
 if [ -n "$TEST" ] ; then
   echo "There are uncommited new files"
 else
   echo "There aren't any uncommited new files"
 fi
-
-# also, maybe optionally, check with the --cached flag too!asd
-#git status --porcelain
