5383c61e1bf6dfcaea4b42b592b717dbf979e1be
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-08-08 17:37:10 +0200

    --staged flag

--- step.sh
+++ step.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Searching for changes in tracked files..."
-git diff --exit-code --quiet --cached
+git diff --exit-code --quiet --staged
 if [ $? = 1 ]
 then
   echo "There are uncommited changes"
 else
   echo "There aren't any uncommited changes"
 fi
-# also, maybe optionally, check with the --cached flag too!
+# also, maybe optionally, check with the --cached flag too!asd
